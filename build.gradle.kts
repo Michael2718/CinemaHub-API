48275606f79b84ee736ccc5fd18cf4ff73968451
Michael <isaevmihail007@gmail.com>
2024-05-07 22:53:44 +0300

    First commit

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+val postgresql_driver_version: String by project
+val exposed_version: String by project
+val h2_version: String by project
+
+plugins {
+    kotlin("jvm") version "1.9.24"
+    id("io.ktor.plugin") version "2.3.10"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.24"
+}
+
+group = "com.michael"
+version = "0.0.1"
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+
+    implementation("org.postgresql:postgresql:$postgresql_driver_version")
+
+    implementation("com.h2database:h2:$h2_version")
+
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
---
+++ src/main/kotlin/com/michael/Application.kt
@@ -0,0 +1,14 @@
+package com.michael
+
+import com.michael.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureDatabases()
+    configureRouting()
+}
